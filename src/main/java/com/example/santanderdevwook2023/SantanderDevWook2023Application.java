92c7ea250667ea11481c91402b80c3e3bdff62f0
devLucasMoraes <devlucasmoraes@gmail.com>
2023-09-30 14:20:52 -0300

    Initial commit

---
+++ src/main/java/com/example/santanderdevwook2023/SantanderDevWook2023Application.java
@@ -0,0 +1,13 @@
+package com.example.santanderdevwook2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWook2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWook2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/santanderdevwook2023/SantanderDevWook2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santanderdevwook2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWook2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
